bb64f599aedd8cb3e7729f67156107bcf7b68698
matsub <matsub.rk@gmail.com>
2015-07-04 02:01:21 +0900

    fix markdown.js
    HTTPRequestが一つだったとき、argumentsがResponseをリストにして渡さないバグ（仕様？）に対応
    
    argumentsはリストである事は保証されるが、applyしたiterable callableに対する返り値のリストであることを保証しない？
    
    あるいはwhen.apply($, iterable)が一度のループで終了した場合に、リストを返さない？
    
    どちらにせよ引数の形式によって返り値のObject Typeが変わるとかどんな設計なんだ

--- markdown.js
+++ markdown.js
@@ -1,5 +1,6 @@
 $(function(){
-	// マークダウン変換
+	/** マークダウン変換 **/
+	// reqとidをバインドした上手いやり方が欲しい
 	ids = [];
 	reqs = [];
 	$.each($("div.markdown"), function(index, Node){
@@ -13,7 +14,15 @@ $(function(){
 			$target.html( marked( markdown ) );
 		}
 	});
-	if (reqs.length > 0) {
+	// argumentsのクソ仕様対応
+	if (reqs.length == 1){
+		reqs[0].done(function(Obj){
+			$target = $("div#"+ids[0]);
+			markdown = Obj;
+			$target.html(marked(markdown));
+		});
+	} else if (reqs.length > 1) {
+		// 各HTTPRequestの結果を受けてmarkdownを書き込む
 		$.when.apply($, reqs).done(function(){
 			$.each(arguments, function(index, Obj){
 				$target = $("div#"+ids[index]);
