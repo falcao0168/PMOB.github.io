42958048c8347846dd000d2dfae64418f1c3250d
matsub <matsub.rk@gmail.com>
2016-03-05 19:03:41 +0900

    :cleanup: 許して

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
 $(function(){
-	/* 活動記録引っ張ってくる */
-	// 外部ソース埋め込み
-	$.getJSON( "event/index.json", function(data){
-		shownArticles = 0;
-		now = Math.floor( $.now() / 1000 );
-		$.each(data, function(index, article){
-			if(article.deadline > now){ // 期限前
-				// 記事引っ張ってきてmarkdown変換
-				$tar = $("#notice");
-				$.get( "event/"+article.fname, markup_source( $tar ) );
+	/* デザイン系 */
+	// 戻るボタンつくる
+	$backLink = $("<a></a>");
+	$backLink.addClass("ghost");
+	$backLink.addClass("smooth");
+	$backLink.attr("href", "#");
+	$backLink.html("あの頃にもどる");
+	$("article").append( $backLink );
 
-				// 掲載日時の表示
-				$time = $("<time></time>");
-				deadline = new Date(article.deadline * 1000);
-				$time.html("-- " + deadline.toDateString());
-				$tar.prepend($time);
-			}
-			else if(shownArticles < 7) { // 期限杉
-				// 過去ログ行き上位7つキャプションと日時だけ表示
-				$tar = $("#history");
-				$p = $("<p></p>");
-				deadline = new Date(article.deadline * 1000);
-				$p.html(deadline.toDateString() + " -- " + article.caption);
-				$tar.prepend($p);
-				$h1 = $("<h1></h1>");
-				$h1.html("活動記録");
-				$tar.prepend($h1);
-				shownArticles += 1;
-			}
-		});
+	// リストぬるってする
+	$("ul.top > li").on({
+		"mouseenter": function(evnt){
+			$target = $( $(this).children("a")[1] );
+			$target.animate({top: "0px"}, 200);
+		},
+		"mouseleave": function(evnt){
+			$target = $( $(this).children("a")[1] );
+			$target.animate({top: "40px"}, 200);
+		}
+	});
+
+	// スムーズスクロール
+	$("a.smooth").click(function(){
+		ref = $(this).attr("href");
+		target = $(ref == "#" || ref == "" ? "html" : ref);
+		pos = target.offset().top;
+		if (pos > 24) pos -= 24;
+		$("body, html").animate({scrollTop: pos}, 420);
+		return false;
 	});
 
 	/* メンバー引っ張ってくる */
